f8024c28e57034de60ed729bc33e2bf1dab0089d
Mohammed Mamun <momamun@yahoo.com>
2020-06-15 15:22:43 -0400

    controller fix

--- src/main/java/com/springframework/springwebapp/controllers/BookController.java
+++ src/main/java/com/springframework/springwebapp/controllers/BookController.java
@@ -18,6 +18,6 @@ public class BookController {
     public String getBooks(Model model) {
 
         model.addAttribute("books", bookRepo.findAll());
-        return "books/list.html";
+        return "books/list";
     }
 }
